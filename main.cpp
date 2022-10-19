04355aef9434dac0141f7ddb4ac89c3c53132fa7
Chu-4hun <tapok134224@mail.ru>
2022-10-19 21:30:53 +0300

    Finally normal libs install

--- main.cpp
+++ main.cpp
@@ -1,35 +1,34 @@
-#include "glad/glad.h"
+#include "Gl/glew.h"
 #include <GLFW/glfw3.h>
 
 #include "iostream"
 
-int main(void)
-{
-    GLFWwindow* window;
+int main(void) {
+
+    GLFWwindow *window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+
+    GLenum err;
+    if ((err = glewInit()) != GLEW_OK)
+        std::cout << glewGetErrorString(err) << std::endl;
 
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
